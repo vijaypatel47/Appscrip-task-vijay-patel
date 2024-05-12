e8ebdb3a62c1df1623da21d7cdce44bee3bdeace
vijay <136144492+vijaypatel47@users.noreply.github.com>
2024-05-12 22:34:19 +0530

    index.js

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,18 +1,20 @@
+
 import './index.css'
 
 const ProductItem = props => {
-  const {blogData} = props
+  const {blogData, isHeartActive} = props
   const {title, image} = blogData
+
+  const active = isHeartActive
+    ? 'https://res.cloudinary.com/drgheojrx/image/upload/v1715519592/heart_rylx1i.png'
+    : 'https://res.cloudinary.com/drgheojrx/image/upload/v1715454035/heart_nioema.png'
+
   return (
     <li className="item-cont">
       <img src={image} alt="title" className="product-img" />
       <div className="name-heart-container">
         <h4 className="product-name">{title}</h4>
-        <img
-          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715454035/heart_nioema.png"
-          className="heart"
-          alt="heart"
-        />
+        <img src={active} className="heart" alt="heart" />
       </div>
     </li>
   )
