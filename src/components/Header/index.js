a14b43fd2c8232979b35ad5050019100718f9abd
vijay <136144492+vijaypatel47@users.noreply.github.com>
2024-05-12 22:32:48 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -43,7 +43,41 @@ const Header = () => (
       <li className="section-name">ABOUT</li>
       <li className="section-name">CONTACT US</li>
     </ul>
+
+    <nav className="mobile-view-container">
+      <div className="logos-cont">
+        <img
+          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715525346/solar_hamburger-menu-linear_kkbvvh.png"
+          className="navbar-logo"
+          alt="icon"
+        />
+        <img
+          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715450201/Logo_yzybpy.png"
+          alt="logo"
+          className="navbar-logo"
+        />
+      </div>
+      <h1 className="logo">LOGO</h1>
+      <div className="navbar-icon-cont">
+        <img
+          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715453359/search-normal_gaxqcp.png"
+          className="navbar-icon"
+          alt="icon"
+        />
+        <img
+          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715454035/heart_nioema.png"
+          className="navbar-icon"
+          alt="icon"
+        />
+        <img
+          src="https://res.cloudinary.com/drgheojrx/image/upload/v1715454063/shopping-bag_osfl7b.png"
+          className="navbar-icon"
+          alt="icon"
+        />
+      </div>
+    </nav>
   </>
 )
 
 export default Header
+
